e46e18c409bfa1c0a6ba3c7953677355d3ce1078
babotavo419 <one.faith419@gmail.com>
2023-08-04 16:36:38 -0400

    ok

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,13 +1,51 @@
+const Account = require('./accounts-model');
+
 exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
-  // Note: you can either write "manual" validation logic
-  // or use the Yup library (not currently installed)
+  const { name, budget } = req.body;
+  
+  if(name === undefined || budget === undefined) {
+    next({ status: 400, message: 'name and budget are required' });
+  } else if (typeof name !== 'string') {
+    next({ status: 400, message: 'name of account must be a string' });
+  } else if (name.trim().length < 3 || name.trim().length > 100) {
+    next({ status: 400, message: 'name of account must be between 3 and 100' });
+  } else if (typeof budget !== 'number' || isNaN(budget)) {
+    next({ status: 400, message: 'budget of account must be a number' });
+  } else if (budget < 0 || budget > 1000000) {
+    next({ status: 400, message: 'budget of account is too large or too small' });
+  } else {
+    req.body.name = name.trim();
+    next();
+  }
 }
 
-exports.checkAccountNameUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+exports.checkAccountNameUnique = async (req, res, next) => {
+  try {
+    const existing = await Account.getAll()
+    .then(accounts => accounts.find(acc => acc.name === req.body.name));
+
+    if (existing) {
+      next({ status: 400, message: 'that name is taken' });
+    } else {
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
 }
 
-exports.checkAccountId = (req, res, next) => {
-  // DO YOUR MAGIC
+exports.checkAccountId = async (req, res, next) => {
+  try {
+    const account = await Account.getById(req.params.id);
+
+    if (!account) {
+      next({ status: 404, message: 'account not found' });
+    } else {
+      req.account = account;
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
 }
+
--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,27 @@
+const db = require('../data/db-config'); // adjust the path to your actual db config file
+
 const getAll = () => {
-  // DO YOUR MAGIC
+  return db('accounts'); // returns all records in the 'accounts' table
 }
 
 const getById = id => {
-  // DO YOUR MAGIC
+  return db('accounts').where({ id }).first(); // returns the first record that matches the id
 }
 
-const create = account => {
-  // DO YOUR MAGIC
+const create = async account => {
+  const [id] = await db('accounts').insert(account);
+  return getById(id);
 }
 
-const updateById = (id, account) => {
-  // DO YOUR MAGIC
+const updateById = async (id, account) => {
+  await db('accounts').where({ id }).update(account);
+  return getById(id);
 }
 
-const deleteById = id => {
-  // DO YOUR MAGIC
+const deleteById = async id => {
+  const toBeDeleted = await getById(id);
+  await db('accounts').where({ id }).del();
+  return toBeDeleted;
 }
 
 module.exports = {
@@ -25,3 +31,4 @@ module.exports = {
   updateById,
   deleteById,
 }
+
