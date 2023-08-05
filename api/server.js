bbc84986a8b95f3cfde2882724e33db7159e22b7
babotavo419 <one.faith419@gmail.com>
2023-08-05 13:55:59 -0400

    ok

--- api/server.js
+++ api/server.js
@@ -4,4 +4,10 @@ const server = express();
 
 server.use(express.json());
 
+server.use('*', (req ,res) => {
+    req.statusCode(404).js({
+        message: 'not found'
+    })
+})
+
 module.exports = server;
